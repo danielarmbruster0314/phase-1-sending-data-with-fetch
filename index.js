c6d6564555f10bfa97d38aeff82456b9c9bc2e85
Daniel Armbruster <danielgundam00@gmail.com>
2022-03-03 13:30:08 -0700

    commit tree 0b5bf4da49604df916ae124ff0270eb134219447

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Add your code here
+//construct a POST request using fetch()
+
+// fetch('http://localhost:3000/dogs', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type' : 'application/json',
+//         Accept: 'application/json',
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//     }) 
+//     .then((resp) => resp.json())
+//     .then((obj) => console.log(obj))
+// });
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     });
+
+
+function submitData(username, useremail){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name : username,
+            email : useremail
+        })
+    })
+    .then((resp) => resp.json() )
+    .then((obj) => {
+        document.body.innerHTML = obj.id })
+    .catch( (error) => {
+        document.body.innerHTML = error.message
+    })    
+}
